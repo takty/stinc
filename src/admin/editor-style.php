2fbad4bda095ac2543e71e3ba1cfa02a62e26a36
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-02-13 12:40:25 +0900

    Minor fix

--- src/admin/editor-style.php
+++ src/admin/editor-style.php
@@ -6,7 +6,7 @@ namespace st\editor_style;
  * Editor Styles (PHP)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2018-01-01
+ * @version 2018-02-13
  *
  */
 
@@ -14,7 +14,7 @@ namespace st\editor_style;
 function initialize() {
 	if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) ) {
 		add_filter( 'mce_external_plugins', function ( $plugins  ) {
-			$plugins['columns'] = get_template_directory_uri() .'/lib/stinc/admin/asset/editor-command.js';
+			$plugins['columns'] = get_template_directory_uri() .'/lib/stinc/admin/asset/editor-command.min.js';
 			return $plugins;
 		} );
 		add_filter( 'tiny_mce_before_init', function ( $settings ) {
