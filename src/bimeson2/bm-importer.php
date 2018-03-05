6dc3c73eb9368d62677e5e5fcacb69129164f3db
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-03-05 18:12:12 +0900

    Bug fix

--- src/bimeson2/bm-importer.php
+++ src/bimeson2/bm-importer.php
@@ -6,7 +6,7 @@ namespace st;
  * Bimeson Importer
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2018-02-23
+ * @version 2018-03-05
  *
  */
 
@@ -47,7 +47,7 @@ class Bimeson_Importer extends \WP_Importer {
 	}
 
 	public function dispatch() {
-		wp_enqueue_script( 'bimeson', get_template_directory_uri() . '/lib/stinc/bimeson2/asset/bm-loader.js' );
+		wp_enqueue_script( 'bimeson', get_template_directory_uri() . '/lib/stinc/bimeson2/asset/bm-loader.min.js' );
 		wp_enqueue_script( 'xlsx', get_template_directory_uri() . '/lib/stinc/bimeson2/asset/xlsx.full.min.js' );
 
 		$this->_header();
