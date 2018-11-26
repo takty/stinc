19ae5c245892f7ff0b062f233e3d08bc9168af02
Takty <tokutadaginaya@gmail.com>
2018-11-26 17:15:32 +0900

    Refactoring

--- src/admin/asset/background-image.js
+++ src/admin/asset/background-image.js
@@ -3,129 +3,119 @@
  * Background Images (JS)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2018-01-04
+ * @version 2018-11-26
  *
  */
 
+
 function st_background_image_initialize_admin(key) {
-	var NS            = 'st-background-image';
+	const NS = 'st-background-image';
 
-	var CLS_TABLE     = NS + '-table';
-	var CLS_ITEM      = NS + '-item';
-	var CLS_ITEM_TEMP = NS + '-item-template';
-	var CLS_HANDLE    = NS + '-handle';
-	var CLS_ADD_ROW   = NS + '-add-row';
-	var CLS_ADD       = NS + '-add';
-	var CLS_DEL       = NS + '-delete';
-	var CLS_SEL_IMG   = NS + '-select-img';
-	var CLS_TN_IMG    = NS + '-thumbnail-img';
-	var CLS_MEDIA     = NS + '-media';
-	var CLS_ITEM_DEL  = NS + '-item-deleted';
+	const CLS_TABLE     = NS + '-table';
+	const CLS_ITEM      = NS + '-item';
+	const CLS_ITEM_TEMP = NS + '-item-template';
+	const CLS_ITEM_PH   = NS + '-item-placeholder';
+	const CLS_ITEM_DEL  = NS + '-item-deleted';
+	const CLS_HANDLE    = NS + '-handle';
+	const CLS_DEL       = NS + '-delete';
+	const CLS_SEL_IMG   = NS + '-select-img';
+	const CLS_TN_IMG    = NS + '-thumbnail-img';
+	const CLS_ADD_ROW   = NS + '-add-row';
+	const CLS_ADD       = NS + '-add';
+	const CLS_MEDIA     = NS + '-media';
 
-	var id     = key;
-	var id_hta = key + '-hidden-textarea';
-	var id_hd  = key + '-hidden-div';
+	const STR_ADD = document.getElementsByClassName(CLS_ADD)[0].innerText;
 
-	var count  = document.getElementById(id);
-	var body   = document.querySelector('#' + id + ' + div');
+	const id     = key;
+	const count  = document.getElementById(id);
+	const body   = document.querySelector('#' + id + ' + div');
 
-	var tbl    = body.getElementsByClassName(CLS_TABLE)[0];
-	var items  = tbl.getElementsByClassName(CLS_ITEM);
-	var temp   = tbl.getElementsByClassName(CLS_ITEM_TEMP)[0];
-	var addRow = tbl.getElementsByClassName(CLS_ADD_ROW)[0];
+	const tbl    = body.getElementsByClassName(CLS_TABLE)[0];
+	const items  = tbl.getElementsByClassName(CLS_ITEM);
+	const temp   = tbl.getElementsByClassName(CLS_ITEM_TEMP)[0];
+	const addRow = tbl.getElementsByClassName(CLS_ADD_ROW)[0];
+	const add    = body.getElementsByClassName(CLS_ADD)[0];
 
 	if (count.tagName !== 'INPUT') console.error('The key or id conflicts.');
 
 	jQuery(tbl).sortable();
 	jQuery(tbl).sortable('option', {
 		containment: 'parent',
-		cursor: 'move',
-		handle: '.' + CLS_HANDLE,
-		items: '> .' + CLS_ITEM,
-		placeholder: 'st_background_image_item_placeholder',
-		update: function () {reorder_item_ids();},
+		cursor     : 'move',
+		handle     : '.' + CLS_HANDLE,
+		items      : '> .' + CLS_ITEM,
+		placeholder: CLS_ITEM_PH,
+		update     : reorder_item_ids,
 	});
 
 	reorder_item_ids();
-	for (var i = 0; i < items.length; i += 1) assign_event_listener(items[i]);
+	for (let i = 0; i < items.length; i += 1) assign_event_listener(items[i]);
 
-	var gp = null;
-	var add = tbl.getElementsByClassName(CLS_ADD)[0];
-	add.addEventListener('click', function (e) {
-		e.preventDefault();
-		if (!gp) {
-			gp = create_media(true);
-			gp.on('select', function () {
-				var ms = gp.state().get('selection');
-				ms.each(function (m) {add_new_item(m.toJSON());});
-				reorder_item_ids();
-			});
-		}
-		gp.open();
-	});
+	setMediaPicker(add, false, (target, ms) => {
+		ms.forEach((m) => { add_new_item(m); });
+		reorder_item_ids();
+	}, { multiple: true, type: 'image', title: STR_ADD });
+
+
+	// -------------------------------------------------------------------------
+
 
 	function reorder_item_ids() {
-		for (var i = 0; i < items.length; i += 1) {
-			var media     = items[i].getElementsByClassName(CLS_MEDIA)[0];
-			var del       = items[i].getElementsByClassName(CLS_DEL)[0];
-			var thumbnail = items[i].getElementsByClassName(CLS_TN_IMG)[0];
-			var sel_img   = items[i].getElementsByClassName(CLS_SEL_IMG)[0];
-
-			var idi = id + '_' + i;
-			items[i].id               = idi;
-			media.id     = media.name = idi + '_media';
-			del.id       = del.name   = idi + '_delete';
-			thumbnail.id              = idi + '_thumbnail';
-
-			sel_img.setAttribute('data-id', i);
-		}
+		for (let i = 0; i < items.length; i += 1) set_item_id(i, items[i]);
 		count.value = items.length;
 	}
 
-	function add_new_item(f) {
-		var item = temp.cloneNode(true);
-		item.getElementsByClassName(CLS_MEDIA)[0].value = f.id;
-		item.getElementsByClassName(CLS_TN_IMG)[0].style.backgroundImage = "url('" + f.url + "')";
-		item.classList.remove(CLS_ITEM_TEMP);
-		item.classList.add(CLS_ITEM);
-		tbl.insertBefore(item, addRow);
-		assign_event_listener(item);
+	function set_item_id(i, it) {
+		const idi = id + '_' + i;
+		it.id = idi;
+		set_id_name_by_class(it, CLS_MEDIA,  idi + '_media');
+		set_id_name_by_class(it, CLS_DEL,    idi + '_delete');
+		set_id_name_by_class(it, CLS_TN_IMG, idi + '_thumbnail', true);
+		set_idi(it, [CLS_SEL_IMG], idi);
 	}
 
-	function assign_event_listener(item) {
-		var del = item.getElementsByClassName(CLS_DEL)[0];
-		var sel_img = item.getElementsByClassName(CLS_SEL_IMG)[0];
+	function set_id_name_by_class(parent, cls, id_name, only_id = false) {
+		const elm = parent.getElementsByClassName(cls)[0];
+		elm.id = id_name;
+		if (!only_id) elm.name = id_name;
+	}
+
+	function set_idi(parent, clss, idi) {
+		clss.forEach((cls) => {
+			const elm = parent.getElementsByClassName(cls)[0];
+			elm.setAttribute('data-idi', idi);
+		});
+	}
+
+	function add_new_item(f) {
+		const it = temp.cloneNode(true);
+
+		it.getElementsByClassName(CLS_MEDIA)[0].value = f.id;
+		it.getElementsByClassName(CLS_TN_IMG)[0].style.backgroundImage = "url('" + f.url + "')";
+
+		it.classList.remove(CLS_ITEM_TEMP);
+		it.classList.add(CLS_ITEM);
+
+		tbl.insertBefore(it, addRow);
+		assign_event_listener(it);
+	}
+
+	function assign_event_listener(it) {
+		const del     = it.getElementsByClassName(CLS_DEL)[0];
+		const sel_img = it.getElementsByClassName(CLS_SEL_IMG)[0];
 
 		del.addEventListener('click', function (e) {
 			if (e.target.checked) {
-				item.classList.add(CLS_ITEM_DEL);
+				it.classList.add(CLS_ITEM_DEL);
 			} else {
-				item.classList.remove(CLS_ITEM_DEL);
+				it.classList.remove(CLS_ITEM_DEL);
 			}
 		});
-		var p = null;
-		sel_img.addEventListener('click', function (e) {
-			e.preventDefault();
-			var idi = id + '_' + e.target.getAttribute('data-id');
-			if (!p) {
-				p = create_media(false);
-				p.on('select', function () {
-					var f = p.state().get('selection').first().toJSON();
-					document.getElementById(idi + '_media').value = f.id;
-					document.getElementById(idi + '_thumbnail').style.backgroundImage = 'url(' + f.url + ')';
-				});
-			}
-			p.open();
-		});
-	}
-
-	function create_media(multiple) {
-		return wp.media({
-			title: document.getElementsByClassName(CLS_ADD)[0].innerText,
-			library: {type: 'image'},
-			frame: 'select',
-			multiple: multiple,
-		});
+		setMediaPicker(sel_img, false, function (target, f) {
+			const idi = target.getAttribute('data-idi');
+			document.getElementById(idi + '_media').value = f.id;
+			document.getElementById(idi + '_thumbnail').style.backgroundImage = 'url(' + f.url + ')';
+		}, { multiple: false, type: 'image', title: STR_ADD });
 	}
 
 }
--- src/admin/background-image.php
+++ src/admin/background-image.php
@@ -6,12 +6,13 @@ namespace st;
  * Background Images (PHP)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2018-03-07
+ * @version 2018-11-26
  *
  */
 
 
 require_once __DIR__ . '/../system/field.php';
+require_once __DIR__ . '/../tag/url.php';
 
 
 class BackgroundImage {
@@ -37,37 +38,39 @@ class BackgroundImage {
 	const CLS_TN_IMG    = self::NS . '-thumbnail-img';
 	const CLS_MEDIA     = self::NS . '-media';
 
-	static private $_instance = null;
-	static public function get_instance() { return self::$_instance; }
+	static private $_instance = [];
 
-	private $_key;
-	private $_id;
-	private $_id_hta;
-	private $_id_hd;
-
-	private $_effect_type       = 'fade'; // 'slide' or 'scroll' or 'fade'
-	private $_zoom_rate         = 1;
-	private $_duration_time     = 8; // [second]
-	private $_transition_time   = 1; // [second]
-	private $_is_random_timing  = true;
-
-	public function __construct( $key ) {
-		$this->_key      = $key;
-		$this->_id       = $key;
-		$this->_id_hta   = $key . '-hidden-textarea';
-		$this->_id_hd    = $key . '-hidden-div';
-
-		if ( self::$_instance === null ) self::$_instance = $this;
+	static public function get_instance( $key = false ) {
+		if ( $key === false ) return reset( self::$_instance );
+		if ( isset( self::$_instance[ $key ] ) ) return self::$_instance[ $key ];
+		return new SlideShow( $key );
 	}
 
-	public function enqueue_script( $url_to ) {
+	static public function enqueue_script( $url_to = false ) {
+		if ( $url_to === false ) $url_to = \st\get_file_uri( __DIR__ );
 		$url_to = untrailingslashit( $url_to );
 		if ( is_admin() ) {
-			wp_enqueue_script( self::NS, $url_to . '/asset/background-image.min.js', [ 'jquery-ui-sortable' ] );
+			wp_enqueue_script( 'picker-media', $url_to . '/asset/lib/picker-media.min.js', [], 1.0, true );
+			wp_enqueue_script( self::NS, $url_to . '/asset/background-image.min.js', [ 'picker-media', 'jquery-ui-sortable' ] );
 			wp_enqueue_style(  self::NS, $url_to . '/asset/background-image.min.css' );
 		} else {
 			wp_enqueue_script( self::NS, $url_to . '/../../stomp/background-image/background-image.min.js', '', 1.0 );
 		}
+	}
+
+	private $_key;
+	private $_id;
+
+	private $_effect_type      = 'fade'; // 'slide' or 'scroll' or 'fade'
+	private $_zoom_rate        = 1;
+	private $_duration_time    = 8; // [second]
+	private $_transition_time  = 1; // [second]
+	private $_is_random_timing = true;
+
+	public function __construct( $key ) {
+		$this->_key = $key;
+		$this->_id  = $key;
+		self::$_instance[ $key ] = $this;
 	}
 
 	public function set_effect_type( $type ) {
@@ -100,10 +103,7 @@ class BackgroundImage {
 		return $this;
 	}
 
-	public function echo_background_image( $post_id = false, $size = 'large', $class = '' ) {
-		if ( $post_id === false ) $post_id = get_the_ID();
-		$is = $this->_get_images( $post_id, $size );
-		if ( empty( $is ) ) return false;
+	private function _create_option_str() {
 		$opts = [
 			'effect_type'     => $this->_effect_type,
 			'duration_time'   => $this->_duration_time,
@@ -111,102 +111,125 @@ class BackgroundImage {
 			'zoom_rate'       => $this->_zoom_rate,
 			'random_timing'   => $this->_is_random_timing
 		];
-		$opts_str = json_encode( $opts );
-	?>
-		<section class="<?php echo self::NS . ( empty( $class ) ? '' : ( ' ' . $class ) ) ?>" id="<?php echo "{$this->_id}_$post_id" ?>">
+		return json_encode( $opts );
+	}
+
+	public function echo_background_image( $post_id = false, $size = 'large', $cls = '' ) {
+		if ( $post_id === false ) $post_id = get_the_ID();
+		$its = $this->_get_items( $post_id, $size );
+		if ( empty( $its ) ) return false;
+
+		$dom_id   = "{$this->_id}-$post_id";
+		$dom_cls  = self::NS . ( empty( $cls ) ? '' : ( ' ' . $cls ) );
+		$opts_str = $this->_create_option_str();
+?>
+		<section class="<?php echo $dom_cls ?>" id="<?php echo $dom_id ?>">
 			<div class="<?php echo self::CLS_STRIP ?>">
 				<ul class="<?php echo self::CLS_SLIDES ?>">
-	<?php
-				foreach ( $is as $i ) $this->_echo_image_item( $i['image'], isset( $i['images'] ) ? $i['images'] : false );
-	?>
+<?php
+		foreach ( $its as $it ) {
+			if ( isset( $it['images'] ) ) $this->_echo_image_item( $it );
+		}
+?>
 				</ul>
 			</div>
-			<script>st_background_image_initialize('<?php echo "{$this->_id}_$post_id" ?>', <?php echo $opts_str ?>);</script>
+			<script>st_background_image_initialize('<?php echo $dom_id ?>', <?php echo $opts_str ?>);</script>
 		</section>
-	<?php
+<?php
 		return true;
 	}
 
-	private function _echo_image_item( $img, $imgs ) {
-		if ( $imgs !== false && 2 <= count( $imgs ) ) {
-			$eu_img0 = esc_url( $imgs[0] );
-			$eu_img1 = esc_url( $imgs[1] );
-			echo "<li data-img=\"$eu_img1\" data-img-phone=\"$eu_img0\"></li>";
+	private function _echo_image_item( $it ) {
+		$imgs = $it['images'];
+
+		if ( 2 <= count( $imgs ) ) {
+			$_img0 = esc_url( $imgs[0] );
+			$_img1 = esc_url( $imgs[1] );
+			echo "<li data-img=\"$_img1\" data-img-phone=\"$_img0\"></li>";
 		} else {
-			$eu_img = esc_url( $img );
-			echo "<li data-img=\"$eu_img\"></li>";
+			$_img = esc_url( $img );
+			echo "<li data-img=\"$_img\"></li>";
 		}
 	}
 
 
 	// -------------------------------------------------------------------------
 
-	public function add_meta_box( $label, $screen ) {
-		\add_meta_box( "{$this->_key}_mb", $label, [ $this, '_cb_output_html' ], $screen );
+
+	public function add_meta_box( $label, $screen, $context = 'side' ) {
+		\add_meta_box( "{$this->_key}_mb", $label, [ $this, '_cb_output_html' ], $screen, $context );
 	}
 
 	public function save_meta_box( $post_id ) {
 		if ( ! isset( $_POST[ "{$this->_key}_nonce" ] ) ) return;
 		if ( ! wp_verify_nonce( $_POST[ "{$this->_key}_nonce" ], $this->_key ) ) return;
-		$this->_save_images( $post_id );
+		$this->_save_items( $post_id );
 	}
+
+
+	// -----------------------------------------------------------------------------
+
 
 	public function _cb_output_html( $post ) {  // Private
 		wp_nonce_field( $this->_key, "{$this->_key}_nonce" );
-	?>
-		<input type="hidden" id="<?php echo $this->_id ?>" name="<?php echo $this->_id ?>" value="" />
+		$its = $this->_get_items( $post->ID );
+?>
+		<input type="hidden" <?php \st\field\name_id( $this->_id ) ?> value="" />
 		<div class="<?php echo self::CLS_BODY ?>">
 			<div class="<?php echo self::CLS_TABLE ?>">
-	<?php
-			$this->_output_row( '', '', self::CLS_ITEM_TEMP );
-			foreach ( $this->_get_images( $post->ID ) as $it ) {
-				$this->_output_row( $it['image'], $it['media'], self::CLS_ITEM );
-			}
-	?>
-				<div class="<?php echo self::CLS_ADD_ROW ?>"><a href="javascript:void(0);" class="<?php echo self::CLS_ADD ?> button"><?php echo __( 'Add Media', 'default' ) ?></a></div>
+<?php
+		$this->_output_row( [], self::CLS_ITEM_TEMP );
+		foreach ( $its as $it ) $this->_output_row( $it, self::CLS_ITEM );
+?>
 			</div>
-			<script>st_background_image_initialize_admin('<?php echo $this->_id ?>');</script>
-			<textarea id="<?php echo $this->_id_hta ?>" style="display: none;"></textarea>
-			<div id="<?php echo $this->_id_hd ?>" style="display: none;"></div>
+			<div class="<?php echo self::CLS_ADD_ROW ?>"><a href="javascript:void(0);" class="<?php echo self::CLS_ADD ?> button"><?php _e( 'Add Media', 'default' ) ?></a></div>
+			<script>window.addEventListener('load', function () {
+				st_background_image_initialize_admin('<?php echo $this->_id ?>');
+			});</script>
 		</div>
-	<?php
+<?php
 	}
 
-	private function _output_row( $image, $media, $class ) {
-		$media_style = empty( $image ) ? '' : ' style="background-image: url(' . esc_url( $image ) . ')"';
-	?>
-		<div class="<?php echo $class ?>">
+	private function _output_row( $it, $cls ) {
+		$_img   = isset( $it['image'] ) ? esc_url( $it['image'] )  : '';
+		$_media = isset( $it['media'] ) ? esc_attr( $it['media'] ) : '';
+		$_style = empty( $_img ) ? '' : " style=\"background-image:url($_img)\"";
+?>
+		<div class="<?php echo $cls ?>">
 			<div>
-				<label class="widget-control-remove <?php echo self::CLS_DEL_LAB ?>"><input type="checkbox" class="<?php echo self::CLS_DEL ?>"></input><br /><?php echo __( 'Remove', 'default' ) ?></label>
 				<div class="<?php echo self::CLS_HANDLE ?>">=</div>
+				<label class="widget-control-remove <?php echo self::CLS_DEL_LAB ?>"><?php _e( 'Remove', 'default' ) ?><br /><input type="checkbox" class="<?php echo self::CLS_DEL ?>" /></label>
 			</div>
 			<div>
 				<div class="<?php echo self::CLS_TN ?>">
-					<a href="javascript:void(0);" class="frame <?php echo self::CLS_SEL_IMG ?>"><div class="<?php echo self::CLS_TN_IMG ?>"<?php echo $media_style ?>></div></a>
+					<a href="javascript:void(0);" class="frame <?php echo self::CLS_SEL_IMG ?>"><div class="<?php echo self::CLS_TN_IMG ?>"<?php echo $_style ?>></div></a>
 				</div>
 			</div>
-			<input type="hidden" class="<?php echo self::CLS_MEDIA ?>" value="<?php echo esc_attr( $media ) ?>" />
+			<input type="hidden" class="<?php echo self::CLS_MEDIA ?>" value="<?php echo $_media ?>" />
 		</div>
-	<?php
+<?php
 	}
 
 
 	// -------------------------------------------------------------------------
 
-	private function _save_images( $post_id ) {
-		$is = \st\field\get_multiple_post_meta_from_post( $this->_key, [ 'media', 'delete' ] );
-		$is = array_filter( $is, function ( $i ) { return ! $i['delete']; } );
-		$is = array_values( $is );
-		\st\field\update_multiple_post_meta( $post_id, $this->_key, $is, [ 'media' ] );
+
+	private function _save_items( $post_id ) {
+		$its = \st\field\get_multiple_post_meta_from_post( $this->_key, [ 'media', 'delete' ] );
+
+		$its = array_filter( $its, function ( $it ) { return ! $it['delete']; } );
+		$its = array_values( $its );
+
+		\st\field\update_multiple_post_meta( $post_id, $this->_key, $its, [ 'media' ] );
 	}
 
-	private function _get_images( $post_id, $size = 'medium' ) {
-		$is = \st\field\get_multiple_post_meta( $post_id, $this->_key, [ 'media' ] );
+	private function _get_items( $post_id, $size = 'medium' ) {
+		$its = \st\field\get_multiple_post_meta( $post_id, $this->_key, [ 'media' ] );
 
-		foreach ( $is as &$i ) {
-			$i['image'] = '';
-			if ( empty( $i['media'] ) ) continue;
-			$aid = intval( $i['media'] );
+		foreach ( $its as &$it ) {
+			$it['image'] = '';
+			if ( empty( $it['media'] ) ) continue;
+			$aid = intval( $it['media'] );
 
 			if ( is_array( $size ) ) {
 				$imgs = [];
@@ -215,15 +238,26 @@ class BackgroundImage {
 					if ( $img ) $imgs[] = $img[0];
 				}
 				if ( ! empty( $imgs ) ) {
-					$i['images'] = $imgs;
-					$i['image'] = $imgs[ count( $imgs ) - 1 ];
+					$it['images'] = $imgs;
+					$it['image' ] = $imgs[ count( $imgs ) - 1 ];
 				}
 			} else {
 				$img = wp_get_attachment_image_src( $aid, $size );
-				if ( $img ) $i['image'] = $img[0];
+				if ( $img ) {
+					$it['images'] = [ $img[0] ];
+					$it['image' ] = $img[0];
+				}
 			}
 		}
-		return $is;
+		return $its;
 	}
 
 }
+
+
+// -----------------------------------------------------------------------------
+
+
+namespace st\echo_background_image;
+
+
