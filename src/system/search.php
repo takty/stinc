5388a2a1f4166a9e0002acd084271f0e2691e424
Takuto Yanagida <yanagida@stxst.com>
2019-04-02 18:12:19 +0900

    Improve post meta search

--- src/system/search.php
+++ src/system/search.php
@@ -6,7 +6,7 @@ namespace st;
  * Search Function for Custom Fields
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2019-03-16
+ * @version 2019-04-02
  *
  */
 
@@ -20,7 +20,11 @@ class Search {
 	}
 
 
+
+
 	// -------------------------------------------------------------------------
+
+
 
 
 	private $_is_slash_in_query_enabled = false;
@@ -32,9 +36,11 @@ class Search {
 
 	private $_search_rewrite_rules_func = null;
 	private $_request_func = null;
-	private $_posts_clauses_filter_added = false;
 	private $_template_redirect_func = null;
 	private $_pre_get_posts_func = null;
+
+	private $_posts_search_filter_added = false;
+	private $_posts_join_filter_added = false;
 
 	private function __construct() {}
 
@@ -64,14 +70,6 @@ class Search {
 		}
 	}
 
-	public function add_meta_key( $str_or_array ) {
-		if ( is_admin() ) return;
-		$this->ensure_posts_clauses_filter();
-
-		if ( ! is_array( $str_or_array ) ) $str_or_array = [ $str_or_array ];
-		$this->_meta_keys = array_merge( $this->_meta_keys, $str_or_array );
-	}
-
 	public function add_post_type( $str_or_array ) {
 		if ( is_admin() ) return;
 		$this->ensure_pre_get_posts_filter();
@@ -88,8 +86,25 @@ class Search {
 		$this->_slug_to_pts[ trim( $slug, '/' ) ] = $post_type_s;
 	}
 
+	public function set_post_meta_search_enabled( $enabled ) {
+		$this->ensure_posts_search_filter();
+		$this->ensure_posts_join_filter();
+	}
+
+	public function add_meta_key( $str_or_array ) {
+		$this->ensure_posts_search_filter();
+		$this->ensure_posts_join_filter();
+
+		if ( ! is_array( $str_or_array ) ) $str_or_array = [ $str_or_array ];
+		$this->_meta_keys = array_merge( $this->_meta_keys, $str_or_array );
+	}
+
+
+
 
 	// Private Functions -------------------------------------------------------
+
+
 
 
 	private function ensure_search_rewrite_rules_filter_added() {
@@ -128,21 +143,30 @@ class Search {
 		$this->_template_redirect_func = null;
 	}
 
-	private function ensure_posts_clauses_filter() {
-		if ( $this->_posts_clauses_filter_added ) return;
-		$this->_stop_words = $this->_get_search_stopwords();
-		add_filter( 'posts_clauses', [ $this, '_cb_posts_clauses' ], 20, 1 );
-		$this->_posts_clauses_filter_added = true;
-	}
-
 	private function ensure_pre_get_posts_filter() {
 		if ( $this->_pre_get_posts_func ) return;
 		$this->_pre_get_posts_func = [ $this, '_cb_pre_get_posts' ];
 		add_filter( 'pre_get_posts', $this->_pre_get_posts_func );
 	}
 
+	private function ensure_posts_search_filter() {
+		if ( $this->_posts_search_filter_added ) return;
+		add_filter( 'posts_search', [ $this, '_cb_posts_search' ], 10, 2 );
+		$this->_posts_search_filter_added = true;
+	}
+
+	private function ensure_posts_join_filter() {
+		if ( $this->_posts_join_filter_added ) return;
+		add_filter( 'posts_join', [ $this, '_cb_posts_join' ], 10, 2 );
+		$this->_posts_join_filter_added = true;
+	}
+
+
+
 
 	// Callback Functions ------------------------------------------------------
+
+
 
 
 	public function _cb_add_rewrite_rules( $rewrite_rules ) {
@@ -208,29 +232,6 @@ class Search {
 		return $query_vars;
 	}
 
-	public function _cb_posts_clauses( $pieces ) {
-		if ( ! is_search() || is_admin() || empty( $this->_meta_keys ) ) return $pieces;
-
-		$q_s    = get_query_var( 's' );
-		$q_sent = get_query_var( 'sentence' );
-
-		$terms = $this->_parse_search( $q_s, $q_sent );
-		$query = '';
-
-		foreach ( $this->_meta_keys as $key ) {
-			foreach ( $terms as $term ) {
-				$query .= "((stspm.meta_key = '{$key}') AND (stspm.meta_value LIKE '%{$term}%')) OR ";
-			}
-		}
-		if ( ! empty( $query ) ) {
-			global $wpdb;
-			$pieces['distinct'] = 'DISTINCT';
-			$pieces['where'] = ' AND ' . $query . '1=1' . $pieces['where'];
-			$pieces['join'] = $pieces['join'] . " INNER JOIN {$wpdb->postmeta} AS stspm ON ({$wpdb->posts}.ID = stspm.post_id)";
-		}
-		return $pieces;
-	}
-
 	public function _cb_pre_get_posts( $query ) {
 		if ( $query->is_search ) {
 			$val = $query->get( 'post_type' );
@@ -241,52 +242,59 @@ class Search {
 		return $query;
 	}
 
+	public function _cb_posts_search( $search, $query ) {
+		if ( ! $query->is_search() || ! $query->is_main_query() ) return $search;
+
+		$q = $query->query_vars;
+		global $wpdb;
+		$search = '';
+
+		$n = ! empty( $q['exact'] ) ? '' : '%';
+		$searchand = '';
+		$exclusion_prefix = apply_filters( 'wp_query_search_exclusion_prefix', '-' );
+
+		foreach ( $q['search_terms'] as $term ) {
+			$exclude = $exclusion_prefix && ( $exclusion_prefix === substr( $term, 0, 1 ) );
+			if ( $exclude ) {
+				$like_op  = 'NOT LIKE';
+				$andor_op = 'AND';
+				$term     = substr( $term, 1 );
+			} else {
+				$like_op  = 'LIKE';
+				$andor_op = 'OR';
+			}
+			$like = $n . $wpdb->esc_like( $term ) . $n;
+			// Add post_meta
+			$search .= $wpdb->prepare( "{$searchand}(($wpdb->posts.post_title $like_op %s) $andor_op ($wpdb->posts.post_content $like_op %s) $andor_op (stinc_search.meta_value $like_op %s))", $like, $like, $like );
+			$searchand = ' AND ';
+		}
+		if ( ! empty( $search ) ) {
+			$search = " AND ({$search}) ";
+			if ( ! is_user_logged_in() ) $search .= " AND ($wpdb->posts.post_password = '') ";
+		}
+		return $search;
+	}
+
+	public function _cb_posts_join( $join, $query ) {
+		if ( ! $query->is_search() || ! $query->is_main_query() ) return $join;
+
+		global $wpdb;
+		$join .= " INNER JOIN ( ";
+		$join .= " SELECT post_id, meta_value FROM $wpdb->postmeta ";
+		if ( ! empty( $this->_meta_keys ) ) {
+			$join .= " WHERE meta_key IN ( '" . implode( "', '", $this->_meta_keys ) . "' ) ";
+		}
+		$join .= " ) AS stinc_search ON ($wpdb->posts.ID = stinc_search.post_id) ";
+		return $join;
+	}
+
+
+
 
 	// Private Functions -------------------------------------------------------
 
 
-	private function _parse_search( $q_s, $q_sent ) {
-		global $wp_the_query;
 
-		$q_s = stripslashes( $q_s );
-		if ( empty( $_GET['s'] ) && $wp_the_query->is_main_query() ) $q_s = $this->urldecode( $q_s );
-		$q_s = str_replace( ["\r", "\n"], '', $q_s );
-
-		$terms = [];
-		if ( ! empty( $q_sent ) ) {
-			$terms = [$q_s];
-		} else {
-			if ( preg_match_all( '/".*?("|$)|((?<=[\t ",+])|^)[^\t ",+]+/', $q_s, $matches ) ) {
-				$terms = $this->_parse_search_terms( $matches[0] );
-				if ( empty( $terms ) || count( $terms ) > 9 ) $terms = [$q_s];
-			} else {
-				$terms = [$q_s];
-			}
-		}
-		return $terms;
-	}
-
-	private function _parse_search_terms( $terms ) {
-		$strtolower = function_exists( 'mb_strtolower' ) ? 'mb_strtolower' : 'strtolower';
-		$checked = [];
-
-		foreach ( $terms as $term ) {
-			if ( preg_match( '/^".+"$/', $term ) ) {
-				$term = trim( $term, "\"'" );
-			} else {
-				$term = trim( $term, "\"' " );
-			}
-			if ( ! $term || ( 1 === strlen( $term ) && preg_match( '/^[a-z\-]$/i', $term ) ) ) continue;
-			if ( in_array( call_user_func( $strtolower, $term ), $this->_stop_words, true ) ) continue;
-			$checked[] = $term;
-		}
-		return $checked;
-	}
-
-	private function _get_search_stopwords() {
-		$stopwords = explode( ',', 'about,an,are,as,at,be,by,com,for,from,how,in,is,it,of,on,or,that,the,this,to,was,what,when,where,who,will,with,www' );
-		return $stopwords;
-	}
 
 	private function urlencode( $str ) {
 		if ( $this->_is_slash_in_query_enabled ) {
