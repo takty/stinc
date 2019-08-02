66a71fec40fbd14995e4eb3aa74bd475ec00aed9
Takuto Yanagida <yanagida@stxst.com>
2019-08-03 08:51:08 +0900

    Add quick tags

--- src/admin/editor-style.php
+++ src/admin/editor-style.php
@@ -6,7 +6,7 @@ namespace st\editor_style;
  * Editor Styles (PHP)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2019-03-07
+ * @version 2019-08-03
  *
  * TinyMCE Advanced Setting:
  * {"settings":{"toolbar_1":"formatselect,bold,italic,underline,strikethrough,superscript,subscript,bullist,numlist,alignleft,aligncenter,alignright,link,unlink","toolbar_2":"undo,redo,styleselect,removeformat,forecolor,backcolor","toolbar_3":"","toolbar_4":"","toolbar_classic_block":"formatselect,bold,italic,blockquote,bullist,numlist,alignleft,aligncenter,alignright,link,forecolor,backcolor,table,wp_help","toolbar_block":"core\/bold,core\/italic,core\/link,tadv\/removeformat","toolbar_block_side":[],"panels_block":"","options":"menubar_block,menubar,merge_toolbars,advlist","plugins":"table,advlist"},"admin_settings":{"options":"hybrid_mode,classic_paragraph_block,no_autop","disabled_editors":""}}
@@ -27,6 +27,7 @@ function initialize( $url_to = false, $row_index = 2 ) {
 		$plugins['columns'] = $url_to . '/asset/editor-command.min.js';
 		return $plugins;
 	} );
+
 	add_filter( 'tiny_mce_before_init', function ( $settings ) {
 		$formats = [];
 		if ( isset( $settings['style_formats'] ) ) {
@@ -65,6 +66,7 @@ function initialize( $url_to = false, $row_index = 2 ) {
 		$settings['style_formats'] = json_encode( $formats );
 		return $settings;
 	} );
+
 	add_filter( "mce_buttons_$row_index", function ( $buttons ) {
 		$buttons[] = 'styleselect';
 		$buttons[] = 'column_2';
@@ -72,4 +74,17 @@ function initialize( $url_to = false, $row_index = 2 ) {
 		$buttons[] = 'column_4';
 		return $buttons;
 	}, 10 );
+
+	add_action( 'admin_print_footer_scripts', function () {
+		if ( wp_script_is( 'quicktags' ) ) {
+?>
+		<script>
+		QTags.addButton('qt-small', 'small', '<small>', '</small>');
+		QTags.addButton('qt-h4', 'h4', '<h4>', '</h4>');
+		QTags.addButton('qt-h5', 'h5', '<h5>', '</h5>');
+		QTags.addButton('qt-h6', 'h6', '<h6>', '</h6>');
+		</script>
+<?php
+		}
+	} );
 }
