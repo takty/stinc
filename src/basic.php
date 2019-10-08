40616f65f1068d1bc057c4e93bee6bbdfcccd4a9
Takuto Yanagida <yanagida@stxst.com>
2019-10-08 11:09:05 +0900

    Add utility

---
+++ src/basic.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/basic/antispam.php';
+require_once __DIR__ . '/basic/blocker.php';
+require_once __DIR__ . '/basic/customizer.php';
+require_once __DIR__ . '/basic/no-emoji.php';
