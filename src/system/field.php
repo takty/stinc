ab5fcffd2d558365cdfea7071318d425bb422b29
Takuto Yanagida <yanagida@stxst.com>
2019-12-26 10:17:43 +0900

    Fix name space bugs

--- src/system/field.php
+++ src/system/field.php
@@ -6,7 +6,7 @@ namespace st\field;
  * Custom Field Utilities
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2019-10-08
+ * @version 2019-12-26
  *
  */
 
@@ -26,7 +26,7 @@ add_action( 'admin_enqueue_scripts', function () {
 function get_post_meta_date( $post_id, $key ) {
 	$ml = \st\Multilang::get_instance();
 
-	$val = mb_trim( get_post_meta( $post_id, $key, true ) );
+	$val = \st\mb_trim( get_post_meta( $post_id, $key, true ) );
 	$val = mysql2date( $ml->get_date_format(), $val );
 
 	return $val;
@@ -35,9 +35,9 @@ function get_post_meta_date( $post_id, $key ) {
 function get_post_meta_lines( $post_id, $key ) {
 	$ml = \st\Multilang::get_instance();
 
-	$val  = mb_trim( get_post_meta( $post_id, $key, true ) );
+	$val  = \st\mb_trim( get_post_meta( $post_id, $key, true ) );
 	$vals = explode( "\n", $val );
-	$vals = array_map( 'mb_trim', $vals );
+	$vals = array_map( '\st\mb_trim', $vals );
 	$vals = array_filter( $vals, function ( $e ) { return ! empty( $e ); } );
 	$vals = array_values( $vals );
 
