2d7cb4ef8a16732d17f83852862c83c0abf4b4ed
Takuto Yanagida <yanagida@stxst.com>
2022-01-10 13:39:03 +0900

    Refactoring

--- src/tag/image.php
+++ src/tag/image.php
@@ -3,10 +3,55 @@
  * Custom Template Tags for Responsive Images
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2021-03-22
+ * @version 2022-01-08
  */
 
 namespace st;
+
+function update_media_options() {
+	update_option( 'thumbnail_size_w', 320 );
+	update_option( 'thumbnail_size_h', 320 );
+	update_option( 'thumbnail_crop', 1 );
+	update_option( 'medium_size_w', 640 );
+	update_option( 'medium_size_h', 9999 );
+	update_option( 'medium_large_size_w', 960 );
+	update_option( 'medium_large_size_h', 9999 );
+	update_option( 'large_size_w', 1280 );
+	update_option( 'large_size_h', 9999 );
+	update_option( 'uploads_use_yearmonth_folders', 1 );
+}
+
+function enable_default_image_sizes( $add_medium_small = true ) {
+	add_image_size( 'small', 320, 9999 );
+	add_image_size( 'huge', 2560, 9999 );
+	if ( $add_medium_small ) {
+		add_image_size( 'medium-small', 480, 9999 );
+	}
+	add_filter(
+		'image_size_names_choose',
+		function ( $sizes ) use ( $add_medium_small ) {
+			$is_ja = preg_match( '/^ja/', get_locale() );
+			$ns    = array();
+			foreach ( $sizes as $idx => $s ) {
+				$ns[ $idx ] = $s;
+				if ( 'thumbnail' === $idx ) {
+					$ns['small'] = ( $is_ja ? '小' : 'Small' );
+					if ( $add_medium_small ) {
+						$ns['medium-small'] = ( $is_ja ? 'やや小' : 'Medium Small' );
+					}
+				}
+				if ( 'medium' === $idx ) {
+					$ns['medium_large'] = ( $is_ja ? 'やや大' : 'Medium Large' );
+				}
+			}
+			return $ns;
+		}
+	);
+}
+
+
+// -----------------------------------------------------------------------------
+
 
 function get_thumbnail_src( $size = 'large', $post_id = false, $meta_key = false ) {
 	$tid = get_thumbnail_id( $post_id, $meta_key );
